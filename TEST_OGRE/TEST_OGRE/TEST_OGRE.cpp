5a6b3421d65e3f4010bd8bb8586c5364333e58af
aksenovdeca <aleksander.aksenov@decagames.com>
2024-09-23 10:50:27 +0300

    Octree Scene added

--- TEST_OGRE/TEST_OGRE/TEST_OGRE.cpp
+++ TEST_OGRE/TEST_OGRE/TEST_OGRE.cpp
@@ -10,6 +10,9 @@
 #include"include/OgreOctree.h"
 #include"include/OgreOctreeNode.h"
 #include"include/OgreOctreeSceneQuery.h"
+#include"MeshLod.h"
+#include"NewInstancing.h"
+
 
 
 class KeyHandler : public OgreBites::InputListener
@@ -42,7 +45,7 @@ int main(int argc, char* argv[])
 	Ogre::AxisAlignedBox box(Ogre::Vector3(-2000, -2000, -2000), Ogre::Vector3(2000, 2000,2000));
 
 
-	scnMgr->init(box, 3000);
+	scnMgr->init(box, 6000);
 
 	Ogre::OctreeNode* root_oct_tree=dynamic_cast<Ogre::OctreeNode*>(scnMgr->getRootSceneNode());
 	
@@ -74,7 +77,7 @@ int main(int argc, char* argv[])
 	
 	// also need to tell where we are
 	Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
-	camNode->setPosition(200, 300, 300);
+	camNode->setPosition(600, 1000, 1000);
 	camNode->lookAt(Ogre::Vector3(-1, 0, 1), Ogre::Node::TS_PARENT);
 
 	// create the camera
@@ -87,9 +90,15 @@ int main(int argc, char* argv[])
 	ctx.getRenderWindow()->addViewport(cam);
 
 	// finally something to render
-	Ogre::Entity* ent = scnMgr->createEntity("ninja.mesh");
-	Ogre::Entity* ent_2 = scnMgr->createEntity("ninja.mesh");
-	Ogre::Entity*ent_3=scnMgr->createEntity("ninja.mesh");
+	Ogre::Entity* ent = scnMgr->createEntity("B17.mesh");
+	Ogre::Entity* ent_2 = scnMgr->createEntity("B17.mesh");
+	Ogre::Entity*ent_3=scnMgr->createEntity("B17.mesh");
+
+	Ogre::Entity*array_of_planes[1000];
+	Ogre::MaterialManager materia_mgr;
+	
+
+	InstanceManager
 	//Ogre::SceneNode* node = scnMgr->getRootSceneNode()->createChildSceneNode();
 	//node->attachObject(ent);
 
@@ -112,14 +121,17 @@ int main(int argc, char* argv[])
 	
 	Ogre::SceneNode*first_ent_node=root_oct_tree->createChildSceneNode("Second Node");
 
-	root_oct_tree->attachObject(groundEntity);
+	for (int i = 0; i < 1000; i++)
+	{
+	    
+		Ogre::InstanceManager::createInstancedEntity()
 
-	auto root_oct_tree_downcasted=static_cast<Ogre::SceneNode*>(root_oct_tree);
-	
-	root_oct_tree_downcasted->_updateBounds();
-		
-			
-	scnMgr->_updateOctreeNode(static_cast<Ogre::OctreeNode*>(root_oct_tree_downcasted));
+	}
+
+	first_ent_node->attachObject(groundEntity);
+
+	first_ent_node->_updateBounds();
+
 
 	
 	auto ent_node_1=(first_ent_node)->createChildSceneNode("second");
@@ -127,15 +139,14 @@ int main(int argc, char* argv[])
 	first_ent_node->attachObject(ent);
 
 	first_ent_node->setPosition(Ogre::Vector3(0,4,1));
-	
-	
 
 	first_ent_node->_updateBounds();
+	
 
 	Ogre::SceneNode*node_ent_2=ent_node_1->createChildSceneNode();
 
 	node_ent_2->attachObject(ent_2);
-	node_ent_2->setPosition(15,0,20);
+	node_ent_2->setPosition(115,0,200);
 
 	node_ent_2->_updateBounds();
 
@@ -143,57 +154,19 @@ int main(int argc, char* argv[])
 	auto ent_node_3=node_ent_2->createChildSceneNode();
 			
 	ent_node_3->attachObject(ent_3);
-	ent_node_3->setPosition(15,0,20);
+	ent_node_3->setPosition(150,0,200);
 
     node_ent_2->_updateBounds();
 
-	
-
-	Ogre::MeshManager* ptr_mesh=Ogre::MeshManager::getSingletonPtr();
-
-	auto mesh_loaded=ptr_mesh->load("ninja.mesh",Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME);
-	
-	auto buff_mg=Ogre::HardwareBufferManager::getSingletonPtr();
-	
-
-
-	auto buffer_vertex=mesh_loaded->getHardwareBufferManager();
-
-	auto skel_mesh=mesh_loaded->getSkeleton();
-			
-	auto sub_mesh=mesh_loaded->getSubMeshes();
+						
+	Ogre::Ray ray(Ogre::Vector3(0,0,0),Ogre::Vector3(100,-200,145));
 
 	
-
-	auto &vertex_subdata=sub_mesh.at(0)->vertexData;
-
-	vertex_subdata->vertexBufferBinding->getBuffer(0);
-
-	auto buffer_mesh_temp=vertex_subdata->vertexBufferBinding->getBuffer(0);
-	auto mesh_manager_vertex_num=buffer_mesh_temp->getNumVertices();
-	
-	for (uint16_t count = 0; count < mesh_manager_vertex_num; count++)
-	{
-		vertex_subdata->
-
-		vertex_subdata->vertexBufferBinding->getBuffer(0)->readData(vertex_subdata->,)
-
-	}
-	
-	Ogre::StaticGeometry *geom=scnMgr->createStaticGeometry("ninja.mesh");
-	
-	scnMgr->injectMovableObject(ent_3);
-     
-	 Ogre::Ray ray(Ogre::Vector3(0,0,0),Ogre::Vector3(100,-200,145));
-
-	auto scene_visible_objects=scnMgr->getVisibleObjectsBoundsInfo(cam);
-
 	Ogre::VisibleObjectsBoundsInfo info;
+		
+	auto ray_query= scnMgr->createRayQuery(ray,0);
 
-	scnMgr->_findVisibleObjects(cam,&info,true);
-
-	auto ray_query= scnMgr->createRayQuery(ray,1);
-
+	
 	ray_query->execute();
 	 
 
